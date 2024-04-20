9ad723f803793f46377b98c83b52f2b6b3ad79e3
Nicholas FitzRoy-Dale <nicholas@fitzroydale.org>
2024-04-20 16:40:18 +0100

    Initial commit of MRU app switcher

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "appthing",
+    dependencies: [
+        // other dependencies
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(name: "appthing", dependencies: [
+            .product(name: "ArgumentParser", package: "swift-argument-parser"),
+        ]),
+    ]
+)
---
+++ Sources/appthing.swift
@@ -0,0 +1,42 @@
+import ArgumentParser
+import Foundation
+import CoreGraphics
+import AppKit
+
+@main
+struct AppThing: ParsableCommand {
+    @Option(help: "Name of the app.")
+    public var name: String? = nil
+
+    @Argument(help: "Command to run.")
+    public var command: String
+
+    public func activate() {
+        // Get the list of windows
+        let options = CGWindowListOption(arrayLiteral: CGWindowListOption.excludeDesktopElements, CGWindowListOption.optionOnScreenOnly)
+        let windowList = CGWindowListCopyWindowInfo(options, CGWindowID(0))
+        let windows = windowList as NSArray? as! [[String: AnyObject]]
+
+        for window in windows {
+            let candidate_name = window[kCGWindowOwnerName as String]!
+
+            if candidate_name as? String == name {
+                let apps = NSWorkspace.shared.runningApplications
+
+                let id = pid_t(window[kCGWindowOwnerPID as String]! as! Int)
+                let app = apps.filter { $0.processIdentifier == id } .first
+                app?.activate()
+
+                break
+            }
+        }
+    }
+
+    public func run() throws {
+        if command == "activate" {
+            activate()
+        } else {
+            print("Unknown command.")
+        }
+    }
+}
